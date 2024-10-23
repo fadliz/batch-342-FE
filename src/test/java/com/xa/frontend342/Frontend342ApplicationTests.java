18a7e9a254aba811a653424203bc2c05d5f7df93
fadliz <asama.fadli@gmail.com>
2024-10-23 09:04:51 +0700

    second first commit :v

---
+++ src/test/java/com/xa/frontend342/Frontend342ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xa.frontend342;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Frontend342ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
