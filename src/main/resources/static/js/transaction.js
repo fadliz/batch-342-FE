03a5a8299335a960baca5de8e411ac7a74800cc4
fadliz <asama.fadli@gmail.com>
2024-10-28 16:41:46 +0700

    add transaction page and functionality

--- src/main/resources/static/js/transaction.js
+++ src/main/resources/static/js/transaction.js
@@ -1,6 +1,7 @@
 const newOrderBtn = document.getElementById("newOrderBtn");
 const paymentBtn = document.getElementById("paymentBtn");
 const transactionBtn = document.getElementById("transactionBtn");
+const payButton = document.getElementById("payButton");
 let variantData = [];
 
 document.addEventListener("DOMContentLoaded", (event) => {
@@ -13,12 +14,17 @@ function openNewOrderModal() {
 
 function openPaymentModal() {
   $("#paymentModal").modal("show");
+  populatePaymentModal();
+  payButton.onclick = function () {
+    submitPaymentForm();
+  };
 }
 
 function startTransaction() {
   newOrderBtn.classList.remove("disabled");
   transactionBtn.classList.add("disabled");
   populateTable(variantData);
+  generateReference();
 }
 
 function loadVariants() {
@@ -33,23 +39,23 @@ function loadVariants() {
 }
 
 function populateTable(variants) {
-  const tbody = document.getElementById("orderBody");
+  let tbody = document.getElementById("orderBody");
   tbody.innerHTML = "";
 
   variants.forEach((variant, index) => {
-    const row = document.createElement("tr");
+    let row = document.createElement("tr");
     row.classList.add("align-middle");
 
     row.innerHTML = `
           <td>
-              <button class="btn btn-warning" onclick="moveToTable(this, ${variant.id})">
+              <button class="btn btn-warning" id="starBtn" value="${variant.id}" onclick="moveToTable(this, this.value)">
               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star" viewBox="0 0 16 16">
                   <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.56.56 0 0 0-.163-.505L1.71 6.745l4.052-.576a.53.53 0 0 0 .393-.288L8 2.223l1.847 3.658a.53.53 0 0 0 .393.288l4.052.575-2.906 2.77a.56.56 0 0 0-.163.506l.694 3.957-3.686-1.894a.5.5 0 0 0-.461 0z"/>
               </svg>
               </button>
           </td>
           <td >${variant.slug}</td>
-          <td > <p class="fw-bold">${variant.name}</p>${variant.description}</td>
+          <td >${variant.name}</td>
           <td >${variant.price}</td>
           <td >${variant.stock}</td>
         `;
@@ -59,31 +65,193 @@ function populateTable(variants) {
 }
 
 function moveToTable(btn, variantId) {
-  const deleteRow = btn.closest("tr");
-  deleteRow.remove();
+  let deleteRow = btn.closest("tr");
+  deleteRow.classList.add("d-none");
 
-    const tbody = document.getElementById("orderTableInput");
-    const inputRow = document.createElement("tr");
-    inputRow.classList.add("align-middle");
-    let variant = variantData.find(v => v.id === variantId);
-    inputRow.innerHTML = `
+  let tbody = document.getElementById("orderTableInput");
+  let inputRow = document.createElement("tr");
+  inputRow.classList.add("align-middle");
+  let variant = variantData.find((v) => v.id === parseInt(variantId));
+  inputRow.innerHTML = `
           <td >
-            <div class="form-outline mb-4">
-                <label class="form-label" for="name">Name</label>
-                <input required type="text" id="name" name="name" class="form-control" autocomplete="off" value ="${variant.name}"/>
-            </div>
+            <input required disabled type="text" class="form-control form-control-sm" autocomplete="off" placeholder="${
+              variant.name
+            }"/>
           </td>
-          <td >${variant.price}</td>
-          <td></td>
-          <td >${variant.stock}</td>
+          <td >
+            <input required disabled type="text" class="form-control form-control-sm" autocomplete="off" placeholder="${
+              variant.price
+            }"/>
           <td>
-              <button class="btn btn-warning" onclick="moveToTable(this, ${variant.id})">
-              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star" viewBox="0 0 16 16">
-                  <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.56.56 0 0 0-.163-.505L1.71 6.745l4.052-.576a.53.53 0 0 0 .393-.288L8 2.223l1.847 3.658a.53.53 0 0 0 .393.288l4.052.575-2.906 2.77a.56.56 0 0 0-.163.506l.694 3.957-3.686-1.894a.5.5 0 0 0-.461 0z"/>
-              </svg>
+            <input required type="number" id="quantity" name="quantity" class="form-control form-control-sm" autocomplete="off" value="1" min=1
+            placeholder="Input Quantity" onchange="loadAmount(this.value, ${
+              variant.price
+            }, this)" onkeyup="loadAmount(this.value, ${variant.price}, this)"/>
+          </td>
+          <td >
+            <input readonly required type="number" id="price" name="price" class="form-control form-control-sm" autocomplete="off" value="${
+              variant.price
+            }" placeholder="Amount"/>
+          </td>
+          <td>
+              <input required hidden type="number" id="variantId" name="variantId" value="${parseInt(
+                variantId
+              )}"/>
+              <button class="btn btn-danger btn-sm" onclick="moveBack(${variantId}, this)">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" 
+                viewBox="0 0 24 24"><path d="M3 6v18h18v-18h-18zm5 14c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 
+                1-1s1 .448 1 1v10zm5 0c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm5 0c0 .552-.448 
+                1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm4-18v2h-20v-2h5.711c.9 0 1.631-1.099 1.631-2h5.315c0 
+                .901.73 2 1.631 2h5.712z"/></svg>
               </button>
           </td>
         `;
 
-    tbody.appendChild(inputRow);
+  tbody.appendChild(inputRow);
+  loadTotal();
+  checkPaymentBtn();
+}
+
+function moveBack(variantId, delBtn) {
+  let ordersTable = document.querySelectorAll('button[id="starBtn"]');
+  ordersTable.forEach((orders) => {
+    if (orders.value == variantId) {
+      orders.closest("tr").classList.remove("d-none");
+    }
+  });
+  let delTableRow = delBtn.closest("tr");
+  delTableRow.remove();
+  loadTotal();
+  checkPaymentBtn();
+}
+
+function generateReference() {
+  $.ajax({
+    type: "get",
+    url: "http://localhost:9001/api/order-header/ref",
+    contentType: "application/json",
+    success: function (referenceJson) {
+      document.getElementById("generatedReference").value = referenceJson.data;
+    },
+  });
+}
+
+function loadAmount(quantity, price, product) {
+  product.closest("tr").querySelector("#price").value = quantity * price;
+  loadTotal();
+}
+
+function loadTotal() {
+  let quantityInputs = document.querySelectorAll('input[name="quantity"]');
+  let priceInputs = document.querySelectorAll('input[name="price"]');
+  let totalQuantity = 0;
+  let totalAmount = 0;
+  quantityInputs.forEach((quantity) => {
+    totalQuantity += parseInt(quantity.value);
+  });
+
+  priceInputs.forEach((prices) => {
+    totalAmount += parseInt(prices.value);
+  });
+
+  document.getElementById("totalQty").value = totalQuantity;
+  document.getElementById("amount").value = totalAmount;
+}
+
+function checkPaymentBtn() {
+  let currentAmount = document.getElementById("amount").value;
+  if (parseInt(currentAmount) != 0) {
+    paymentBtn.classList.remove("disabled");
+  } else {
+    paymentBtn.classList.add("disabled");
+  }
+}
+
+function getTableData() {
+  let table = document.getElementById("orderTable");
+  let data = [];
+  let arr = Array.from(table.rows);
+  arr.forEach((row, index) => {
+    if (index === 0 || index === arr.length - 1) return;
+    let rowData = {};
+    let cells = row.querySelectorAll("td input"); // Select only <td> with <input>
+
+    // Loop through each cell to get input values
+    cells.forEach((input) => {
+      if (input.name === "") return;
+      rowData[`${input.name}`] = input.value;
+    });
+    data.push(rowData);
+  });
+  return data;
+}
+
+function populatePaymentModal() {
+  $("#reference").val(document.getElementById("generatedReference").value);
+  $("#bill").val(document.getElementById("amount").value);
+}
+
+function loadChange(payment) {
+  let bill = $("#bill").val();
+  $("#change").val(payment - bill);
+}
+
+function submitPaymentForm() {
+  createOrderHeader("POST", "http://localhost:9001/api/order-header");
+  
+}
+
+function createOrderHeader(method, url) {
+  const formData = new FormData(document.getElementById("paymentForm"));
+
+  formData.append("createdBy", "Admin");
+  const data = Object.fromEntries(formData.entries());
+  if (data.deleted === undefined) {
+    data.deleted = false;
+  } else {
+    data.deleted = true;
+  }
+  const jsonData = JSON.stringify(data);
+  $.ajax({
+    type: method,
+    url: url,
+    data: jsonData,
+    contentType: "application/json",
+    success: function (response) {
+      createOrderDetail("POST", "http://localhost:9001/api/order-detail/orders", response.data);
+    },
+    error: function (xhr, status, error) {
+      console.log(xhr.responseText);
+      alert("Error submitting form");
+    },
+  });
+}
+
+function createOrderDetail(method, url, createdOrderHeader) {
+  const tabData = getTableData().entries();
+  let orderDetails = [];
+  tabData.forEach((data) => {
+    data[1]["createdBy"] = createdOrderHeader.createdBy;
+    data[1]["headerId"] = createdOrderHeader.id;
+    console.log(data[1]);
+    orderDetails.push(data[1]);
+  });
+  const jsonData = {
+    orderDetails,
+  };
+  console.log(JSON.stringify(jsonData));
+  $.ajax({
+    type: method,
+    url: url,
+    data: JSON.stringify(jsonData),
+    contentType: "application/json",
+    success: function (response) {
+      console.log(response);
+      window.location.reload();
+    },
+    error: function (xhr, status, error) {
+      console.log(xhr.responseText);
+      alert("Error submitting form");
+    },
+  });
 }
